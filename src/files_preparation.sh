a7b1b9d8f2d64760e17a86130deeb554d1830126
ethangr <ethangr@lcg.unam.mx>
2024-05-04 08:26:37 -0600

    modification to save only useful info and test

--- src/files_preparation.sh
+++ src/files_preparation.sh
@@ -1,10 +1,13 @@
 #! bin/bash
 
-# file cleanning
+# gencode file cleanning containing categories of interest
 grep -v "#" data/gencode.v45.annotation.gtf | cut -f1,3-5 | grep -P "gene|exon|CDS" > results/regions.tsv
 
 # file for testing
 grep "chr21" results/regions.tsv > results/regions_chr21.tsv
 
 # clinvar file cleaning
-grep -v "#" data/clinvar_20240331.vcf | grep "OMIM" > results/clinvar_clean.tsv
+grep -v "#" data/clinvar_20240331.vcf | grep "OMIM" | cut -f1-2 > results/clinvar_clean.tsv
+
+# file for testing
+awk '$1 == "21"' results/clinvar_clean.tsv > results/clinvar_clean_chr21.tsv
