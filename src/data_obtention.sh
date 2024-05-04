d5d39d56e091aa8d211c7eb75aecf581d18defdf
CamilaVillazon <124300331+CamilaVillazon@users.noreply.github.com>
2024-05-03 22:35:26 -0600

    Update data_obtention.sh

---
+++ src/data_obtention.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+
+# ClinVar file with mutation positions
+ wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20240331.vcf.gz
+
+# GenCode human genome annotation version 45
+ wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_45/gencode.v45.annotation.gtf.gz
