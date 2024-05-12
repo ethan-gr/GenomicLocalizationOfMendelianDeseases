936671ec0c5452417539a97773ca69ea3598557b
CamilaVillazon <124300331+CamilaVillazon@users.noreply.github.com>
2024-05-11 20:05:20 -0600

    Update files_preparation.sh

--- src/files_preparation.sh
+++ src/files_preparation.sh
@@ -7,7 +7,7 @@ grep -v "#" data/gencode.v45.annotation.gtf | cut -f1,3-5 | grep -P "gene|exon|C
 grep "chr21" results/regions.tsv > results/regions_chr21.tsv
 
 # clinvar file cleaning
-grep -v "#" data/clinvar_20240331.vcf | grep "OMIM" | cut -f1-2 > results/clinvar_clean.tsv
+ grep -v "#" data/clinvar_20240331.vcf | grep "OMIM" | cut -f1-2 | sort -u > results/clinvar_clean.tsv
 
 # file for testing
 awk '$1 == "21"' results/clinvar_clean.tsv > results/clinvar_clean_chr21.tsv
